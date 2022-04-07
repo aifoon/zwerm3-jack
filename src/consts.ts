2494375c1f87b902f136e0088ee2d80d259e73a8
timdpaep <tim.depaepe@gmail.com>
2022-04-07 18:38:50 +0200

    Added resources to build folder

--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,7 @@ import * as path from 'path';
 const JACKTRIP_DEFAULT_CLIENT_NAME = 'client';
 const JACKTRIP_DEFAULT_VERSION = '1.4.1';
 const JACK_DEFAULT_VERSION = '1.9.19';
-const JACKTRIP_BIN_PATH = path.join(__dirname, '../resources/jacktrip/');
+const JACKTRIP_BIN_PATH = path.join(__dirname, 'resources/jacktrip/');
 const JACK_BASE_PATH_WIN = 'C:\\Program Files (x86)\\Jack\\';
 const JACK_BASE_PATH_DARWIN = '/usr/local/bin';
 const JACK_BASE_PATH_LINUX = '/usr/bin';
